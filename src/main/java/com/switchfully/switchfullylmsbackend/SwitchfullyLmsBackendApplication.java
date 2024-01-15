05be11ef4a4ca8ca0be41bbb4ac376369048edf8
Sandro Orsi <orsi.sandro.0@gmail.com>
2024-01-15 11:53:04 +0100

    initial

---
+++ src/main/java/com/switchfully/switchfullylmsbackend/SwitchfullyLmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.switchfullylmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwitchfullyLmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwitchfullyLmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/switchfully/switchfullylmsbackend/SwitchfullyLmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.switchfullylmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwitchfullyLmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
