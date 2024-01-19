ee70602a92acfab8c671e3304a6c69a574a53e80
Sandro Orsi <orsi.sandro.0@gmail.com>
2024-01-19 10:21:08 +0100

    request user from frontend through token

--- src/main/java/com/switchfully/switchfullylmsbackend/controller/UserController.java
+++ src/main/java/com/switchfully/switchfullylmsbackend/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.switchfully.switchfullylmsbackend.controller;
 
 import com.switchfully.switchfullylmsbackend.dto.user.CreateUserDto;
+import com.switchfully.switchfullylmsbackend.dto.user.UserDto;
 import com.switchfully.switchfullylmsbackend.service.UserService;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/register")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
@@ -17,9 +18,13 @@ public class UserController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping(consumes = "application/json", produces = "application/json")
     public void registerUser(@RequestBody CreateUserDto createUserDto) {
         userService.addUser(createUserDto);
+    }
+
+    @GetMapping()
+    public UserDto getUserByToken(@RequestHeader("Authorization") String bearerToken){
+        return userService.getUserByToken(bearerToken);
     }
 }
--- src/main/java/com/switchfully/switchfullylmsbackend/service/UserService.java
+++ src/main/java/com/switchfully/switchfullylmsbackend/service/UserService.java
@@ -1,23 +1,53 @@
 package com.switchfully.switchfullylmsbackend.service;
 
 import com.switchfully.switchfullylmsbackend.dto.user.CreateUserDto;
+import com.switchfully.switchfullylmsbackend.dto.user.UserDto;
+import com.switchfully.switchfullylmsbackend.entity.AbstractUser;
 import com.switchfully.switchfullylmsbackend.entity.Student;
 import com.switchfully.switchfullylmsbackend.mapper.StudentMapper;
+import com.switchfully.switchfullylmsbackend.mapper.UserMapper;
 import com.switchfully.switchfullylmsbackend.repository.UserRepository;
+import org.json.JSONObject;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
     private final StudentMapper studentMapper;
     private final UserRepository userRepository;
-    public UserService(StudentMapper studentMapper, UserRepository userRepository) {
+    private final UserMapper userMapper;
+    public UserService(StudentMapper studentMapper, UserRepository userRepository, UserMapper userMapper) {
         this.studentMapper = studentMapper;
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
     }
 
     public void addUser(CreateUserDto createUserDto) {
         Student student = studentMapper.mapCreateUserDtoToStudent(createUserDto);
         userRepository.save(student);
+    }
+
+    public UserDto getUserByToken(String bearerToken) {
+        String[] chunks = bearerToken.split("\\.");
+        JSONObject payload = new JSONObject(decode(chunks[1]));
+
+        AbstractUser user = userRepository.findByEmail(payload.get("preferred_username").toString());
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        List<String> roles = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return userMapper.mapAbstractUserToUserDto(user, roles.get(0));
+    }
+
+    private static String decode(String encodedString) {
+        return new String(Base64.getUrlDecoder().decode(encodedString));
     }
 }
